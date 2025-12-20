9f0143e51dc583ec8dddd328d698aed77e6e452e
lumen-sinthetic <vadimsi2105@gmail.com>
2025-12-20 10:47:16 +0500

    snake constant movement and sirection changes added

--- scripts/Snake.cs
+++ scripts/Snake.cs
@@ -1,57 +1,21 @@
 using Godot;
-using System;
-using System.IO;
-using System.Threading;
 
 public partial class Snake : CharacterBody2D
 {
 	const float JumpVelocity = -400.0f;
-	const float Speed = 5f;
-
-	// float DegToRad(float deg)
-	// {
-	// 	return deg * float.Pi / 180;
-	// }
-
-
-
-	void Move()
-	{
-		var vector = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		Position += vector * Speed;
-		Rotation = vector.Angle();
-	}
+	const float Speed = 200f;
+	Vector2 Destination = Vector2.Right;
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Move();
-		// Vector2 velocity = Velocity;
+		var input = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 
-		// // Add the gravity.
-		// if (!IsOnFloor())
-		// {
-		// 	velocity += GetGravity() * (float)delta;
-		// }
+		if (input != Vector2.Zero) Destination = input;
 
-		// // Handle Jump.
-		// if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-		// {
-		// 	velocity.Y = JumpVelocity;
-		// }
+		Velocity = Destination * Speed;
 
-		// // Get the input direction and handle the movement/deceleration.
-		// // As good practice, you should replace UI actions with custom gameplay actions.
-		// Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		// if (direction != Vector2.Zero)
-		// {
-		// 	velocity.X = direction.X * Speed;
-		// }
-		// else
-		// {
-		// 	velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-		// }
+		MoveAndSlide();
 
-		// Velocity = velocity;
-		// MoveAndSlide();
+		Rotation = Destination.Angle();
 	}
 }
