203b3b3956984d706b1a3a2d4a9748e4756b8d04
lumen-sinthetic <vadimsi2105@gmail.com>
2025-12-21 11:46:16 +0500

    todo added

---
+++ TODO.md
@@ -0,0 +1,3 @@
+- Rewritte apple creating, eating and positioning logic from node to scene
+- Reorganize script folder structure
+- Reorganize snake scripts from one to separate for snake parts.
--- scripts/Main.cs
+++ scripts/Main.cs
@@ -1,19 +1,18 @@
 using Godot;
 using System;
+using System.Threading;
 
 public partial class Main : Node2D
 {
+	// [Export]
+	// public Snake Snake;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		var child = GetChild(0);
-		GD.Print("here");
-
-		if (child is Snake snake)
-		{
-			GD.Print("here 2");
-			snake.ActionRequested += OnAppleEaten;
-		}
+		var instance = GetNode("Snake");
+		var snake = instance.GetNode<Snake>("CharacterBody2D");
+		snake.ActionRequested += OnAppleEaten;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,9 +22,13 @@ public partial class Main : Node2D
 
 	void OnAppleEaten()
 	{
-		var appleScene = GD.Load<PackedScene>("res://scenes/partial/apple.tscn");
-		var apple = appleScene.Instantiate<Apple>();
-		apple.InitRandomly();
-		AddChild(apple);
+		// GD.Print("Here");
+		// var appleScene = GD.Load<PackedScene>("res://scenes/partial/apple.tscn");
+		// var scene = appleScene.Instantiate();
+
+
+		// apple.InitRandomly();
+		// AddChild(apple);
 	}
+
 }
